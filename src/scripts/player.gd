aa3becead76774c7f2cbd5d6e2c85dbf02e75f75
Yves Pablo Costa Rêgo <ypc.rego@gmail.com>
2025-11-10 20:33:20 -0300

    Fazendo a movimentação funcionar

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 	Dialogic.timeline_started.connect(_on_timeline_started)
 	Dialogic.timeline_ended.connect(set_physics_process.bind(true))
 	Dialogic.timeline_ended.connect(set_process_input.bind(true))
-	
+
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 
 func _on_dialogic_signal(arg : String) -> void:
